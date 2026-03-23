f034eead471234fa393904b4075ccddbb306340d
carlos roberto <carloseng.jr@gmail.com>
2026-03-22 22:20:48 -0300

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`,
+        redirectTo: `${window.location.origin}/api/auth/callback`,
       },
     })
   }
